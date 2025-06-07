fc8863cba390dd642d390f5ceea673efb13a6a2e
./cosmic <91699202+dotslashCosmic@users.noreply.github.com>
2025-06-06 23:21:15 -0400

    Updated structure

--- src/constants.rs
+++ src/constants.rs
@@ -2,6 +2,7 @@
 pub const COEFFS: [u64; 5] = [17, 23, 37, 41, 47];
 pub const MAGIC: u64 = 0x517CC1B727220A97;
 pub const RATIO: u64 = 0x9E3779B97F4A7C15;
+pub const CONST: u64 = 0x8F983AA82D9FBEDF;
 pub const KEY: u128 = 0x8F983AA82D9FBEDF85B1F4441C295DE3;
 pub const ARX_BITS: [u32; 8] = [5, 11, 23, 31, 35, 43, 51, 63];
 pub const MASK_32: u64 = 0xFFFFFFFF;
@@ -10,4 +11,3 @@ pub const MASK_128: u128 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 pub const DIM: usize = 1024;
 pub const Q_MOD: u64 = 282_429_536_481;
 pub const MAGNITUDE: i64 = 6;
-pub const CONSTANT_H_PLACEHOLDER: u64 = 0x8F983AA82D9FBEDF;
